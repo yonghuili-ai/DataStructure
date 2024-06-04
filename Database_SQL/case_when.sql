1731e9b469b431f316ef5426e48856ae494fcd26
Yonghui Li <liyonghui86@gmail.com>
2024-06-03 19:50:24 -0700

    reform, flat, pivot table using case when, group by, max

--- Database_SQL/case_when.sql
+++ Database_SQL/case_when.sql
@@ -189,3 +189,146 @@ group by 1
 
 
 !! 1783
+
+
+-- leetcode 1699. Number of Calls Between Two Persons !!!
+/*
+Table: Calls
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| from_id     | int     |
+| to_id       | int     |
+| duration    | int     |
++-------------+---------+
+This table does not have a primary key (column with unique values), it may contain duplicates.
+This table contains the duration of a phone call between from_id and to_id.
+from_id != to_id
+ 
+
+Write a solution to report the number of calls and the total call duration between each pair of distinct persons (person1, person2) where person1 < person2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Calls table:
++---------+-------+----------+
+| from_id | to_id | duration |
++---------+-------+----------+
+| 1       | 2     | 59       |
+| 2       | 1     | 11       |
+| 1       | 3     | 20       |
+| 3       | 4     | 100      |
+| 3       | 4     | 200      |
+| 3       | 4     | 200      |
+| 4       | 3     | 499      |
++---------+-------+----------+
+Output: 
++---------+---------+------------+----------------+
+| person1 | person2 | call_count | total_duration |
++---------+---------+------------+----------------+
+| 1       | 2       | 2          | 70             |
+| 1       | 3       | 1          | 20             |
+| 3       | 4       | 4          | 999            |
++---------+---------+------------+----------------+
+Explanation: 
+Users 1 and 2 had 2 calls and the total duration is 70 (59 + 11).
+Users 1 and 3 had 1 call and the total duration is 20.
+Users 3 and 4 had 4 calls and the total duration is 999 (100 + 200 + 200 + 499).
+*/
+-- Solution 1
+with tmp 
+as
+(select from_id as person1, to_id as person2, duration 
+from Calls
+where from_id < to_id
+union all 
+select to_id as person1, from_id as person2, duration 
+from Calls
+where to_id < from_id)
+select person1, person2, count(*) as call_count, sum(duration) as total_duration
+from tmp 
+group by 1, 2
+
+
+-- The other solution
+select 
+case when from_id < to_id then from_id else to_id end as person1,
+case when from_id < to_id then to_id else from_id end as person2,
+count(*) as call_count,
+sum(duration) as total_duration
+from calls
+group by 1, 2
+
+
+
+/*
+Leetcode 1179 https://leetcode.com/problems/reformat-department-table/description/
+
+we could get a revenue for each month by using aggregate function such as SUM, MAX or MIN because these functions ignore the null values.
+
+As the table description, the group of (id, month) is the primary key. Hence, we know there could not be more than two valid revenue values for each month of each id
+
+pivot or flat or reform a table, use this sum/max/min(case when   ) as structure
+*/
+
+-- can be simplified as the next query
+with monthly_tb as 
+(
+    select id, 
+    case when month = 'Jan' then revenue else null end as Jan_Revenue, 
+    case when month = 'Feb' then revenue else null end as Feb_Revenue, 
+    case when month = 'Mar' then revenue else null end as Mar_Revenue, 
+    case when month = 'Apr' then revenue else null end as Apr_Revenue, 
+    case when month = 'May' then revenue else null end as May_Revenue, 
+    case when month = 'Jun' then revenue else null end as Jun_Revenue, 
+    case when month = 'Jul' then revenue else null end as Jul_Revenue, 
+    case when month = 'Aug' then revenue else null end as Aug_Revenue, 
+    case when month = 'Sep' then revenue else null end as Sep_Revenue, 
+    case when month = 'Oct' then revenue else null end as Oct_Revenue, 
+    case when month = 'Nov' then revenue else null end as Nov_Revenue, 
+    case when month = 'Dec' then revenue else null end as Dec_Revenue
+    from department)
+select id, 
+max(Jan_Revenue) as Jan_Revenue,
+max(Feb_Revenue) as Feb_Revenue,
+max(Mar_Revenue) as Mar_Revenue,
+max(Apr_Revenue) as Apr_Revenue,
+max(May_Revenue) as May_Revenue, 
+max(Jun_Revenue) as Jun_Revenue,
+max(Jul_Revenue) as Jul_Revenue, 
+max(Aug_Revenue) as Aug_Revenue,
+max(Sep_Revenue) as Sep_Revenue,
+max(Oct_Revenue) as Oct_Revenue, 
+max(Nov_Revenue) as Nov_Revenue, 
+max(Dec_Revenue) as Dec_Revenue
+from monthly_tb
+group by id 
+
+-- simplified version
+select id, 
+max(case when month = 'Jan' then revenue end) as Jan_Revenue, 
+max(case when month = 'Feb' then revenue end)as Feb_Revenue, 
+max(case when month = 'Mar' then revenue end) as Mar_Revenue, 
+max(case when month = 'Apr' then revenue end) as Apr_Revenue, 
+max(case when month = 'May' then revenue end) as May_Revenue, 
+max(case when month = 'Jun' then revenue end)as Jun_Revenue, 
+max(case when month = 'Jul' then revenue end) as Jul_Revenue, 
+max(case when month = 'Aug' then revenue end) as Aug_Revenue, 
+max(case when month = 'Sep' then revenue end) as Sep_Revenue, 
+max(case when month = 'Oct' then revenue end) as Oct_Revenue, 
+max(case when month = 'Nov' then revenue end) as Nov_Revenue, 
+max(case when month = 'Dec' then revenue end) as Dec_Revenue
+from Department
+group by id 
+
+
+
+
