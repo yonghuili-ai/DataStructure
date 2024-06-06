6e49e9f6d1b9eca38ba2a14dd15636d525c1e319
Yonghui Li <liyonghui86@gmail.com>
2024-06-05 17:31:13 -0700

    sql interview question practice

--- company/meta/DE_prepare.sql
+++ company/meta/DE_prepare.sql
@@ -191,6 +191,116 @@ from sales as s
 left join promotions  as p 
 on s.promotion_id = p.promotion_id) as tmp 
 
+ /*
+ BACKGROUND:
+ 
+ The following schema is a subset of a relational database of a grocery store
+ chain. This chain sells many products of different product classes to its
+ customers across its different stores. It also conducts many different
+ promotion campaigns.
+ 
+ The relationship between the four tables we want to analyze is depicted below:
+ 
+        # sales                                # products
+        +------------------+---------+         +---------------------+---------+
+        | product_id       | INTEGER |>--------| product_id          | INTEGER |
+        | store_id         | INTEGER |    +---<| product_class_id    | INTEGER |
+        | customer_id      | INTEGER |    |    | brand_name          | VARCHAR |
+   +---<| promotion_id     | INTEGER |    |    | product_name        | VARCHAR |
+   |    | store_sales      | DECIMAL |    |    | is_low_fat_flg      | TINYINT |
+   |    | store_cost       | DECIMAL |    |    | is_recyclable_flg   | TINYINT |
+   |    | units_sold       | DECIMAL |    |    | gross_weight        | DECIMAL |
+   |    | transaction_date | DATE    |    |    | net_weight          | DECIMAL |
+   |    +------------------+---------+    |    +---------------------+---------+
+   |                                      |
+   |    # promotions                      |    # product_classes
+   |    +------------------+---------+    |    +---------------------+---------+
+   +----| promotion_id     | INTEGER |    +----| product_class_id    | INTEGER |
+        | promotion_name   | VARCHAR |         | product_subcategory | VARCHAR |
+        | media_type       | VARCHAR |         | product_category    | VARCHAR |
+        | cost             | DECIMAL |         | product_department  | VARCHAR |
+        | start_date       | DATE    |         | product_family      | VARCHAR |
+        | end_date         | DATE    |         +---------------------+---------+
+        +------------------+---------+
+ */
+ /*
+ PROMPT
+ -- The CMO is interested in understanding how the sales of different
+ -- product families are affected by promotional campaigns.
+ -- To do so, for each of the available product families,
+ -- show the total number of units sold,
+ -- as well as the ratio of units sold that had a valid promotion
+ -- to units sold without a promotion,
+ -- ordered by increasing order of total units sold.
+ 
+ 
+ EXPECTED OUTPUT
+ Note: Please use the column name(s) specified in the expected output in your solution.
+ +----------------+------------------+--------------------------------------------------+
+ | product_family | total_units_sold | ratio_units_sold_with_promo_to_sold_without_promo|
+ +----------------+------------------+--------------------------------------------------+
+ | Drink          |          43.0000 |                           0.79166666666666666667 |
+ | Non-Consumable |         176.0000 |                           0.76000000000000000000 |
+ | Food           |         564.0000 |                           0.75155279503105590062 |
+ +----------------+------------------+--------------------------------------------------+
+ 
+ -------------- PLEASE WRITE YOUR SQL SOLUTION BELOW THIS LINE ----------------
+ */
+ 
+
+
+
+
+
+/*
+ BACKGROUND:
+ 
+ The following schema is a subset of a relational database of a grocery store
+ chain. This chain sells many products of different product classes to its
+ customers across its different stores. It also conducts many different
+ promotion campaigns.
+ 
+ The relationship between the four tables we want to analyze is depicted below:
+ 
+        # sales                                # products
+        +------------------+---------+         +---------------------+---------+
+        | product_id       | INTEGER |>--------| product_id          | INTEGER |
+        | store_id         | INTEGER |    +---<| product_class_id    | INTEGER |
+        | customer_id      | INTEGER |    |    | brand_name          | VARCHAR |
+   +---<| promotion_id     | INTEGER |    |    | product_name        | VARCHAR |
+   |    | store_sales      | DECIMAL |    |    | is_low_fat_flg      | TINYINT |
+   |    | store_cost       | DECIMAL |    |    | is_recyclable_flg   | TINYINT |
+   |    | units_sold       | DECIMAL |    |    | gross_weight        | DECIMAL |
+   |    | transaction_date | DATE    |    |    | net_weight          | DECIMAL |
+   |    +------------------+---------+    |    +---------------------+---------+
+   |                                      |
+   |    # promotions                      |    # product_classes
+   |    +------------------+---------+    |    +---------------------+---------+
+   +----| promotion_id     | INTEGER |    +----| product_class_id    | INTEGER |
+        | promotion_name   | VARCHAR |         | product_subcategory | VARCHAR |
+        | media_type       | VARCHAR |         | product_category    | VARCHAR |
+        | cost             | DECIMAL |         | product_department  | VARCHAR |
+        | start_date       | DATE    |         | product_family      | VARCHAR |
+        | end_date         | DATE    |         +---------------------+---------+
+        +------------------+---------+
+ */
+ /*
+ PROMPT:
+ -- The VP of Sales feels that some product categories don't sell
+ -- and can be completely removed from the inventory.
+ -- As a first pass analysis, they want you to find what percentage
+ -- of product categories have never been sold.
+ 
+ EXPECTED OUTPUT:
+ Note: Please use the column name(s) specified in the expected output in your solution.
+ +-----------------------------------+
+ | pct_product_categories_never_sold |
+ +-----------------------------------+
+ |               13.8888888888888889 |
+ +-----------------------------------+
+
+ -------------- PLEASE WRITE YOUR SQL SOLUTION BELOW THIS LINE ----------------
+ */
 
 
 
@@ -290,3 +400,149 @@ join best_selling_product b
 on s.store_id = b.store_id
 where store_sales = max_store_sales
 )
+
+
+ /*
+ BACKGROUND:
+ 
+ The following schema is a subset of a relational database of a grocery store
+ chain. This chain sells many products of different product classes to its
+ customers across its different stores. It also conducts many different
+ promotion campaigns.
+ 
+ The relationship between the four tables we want to analyze is depicted below:
+ 
+        # sales                                # products
+        +------------------+---------+         +---------------------+---------+
+        | product_id       | INTEGER |>--------| product_id          | INTEGER |
+        | store_id         | INTEGER |    +---<| product_class_id    | INTEGER |
+        | customer_id      | INTEGER |    |    | brand_name          | VARCHAR |
+   +---<| promotion_id     | INTEGER |    |    | product_name        | VARCHAR |
+   |    | store_sales      | DECIMAL |    |    | is_low_fat_flg      | TINYINT |
+   |    | store_cost       | DECIMAL |    |    | is_recyclable_flg   | TINYINT |
+   |    | units_sold       | DECIMAL |    |    | gross_weight        | DECIMAL |
+   |    | transaction_date | DATE    |    |    | net_weight          | DECIMAL |
+   |    +------------------+---------+    |    +---------------------+---------+
+   |                                      |
+   |    # promotions                      |    # product_classes
+   |    +------------------+---------+    |    +---------------------+---------+
+   +----| promotion_id     | INTEGER |    +----| product_class_id    | INTEGER |
+        | promotion_name   | VARCHAR |         | product_subcategory | VARCHAR |
+        | media_type       | VARCHAR |         | product_category    | VARCHAR |
+        | cost             | DECIMAL |         | product_department  | VARCHAR |
+        | start_date       | DATE    |         | product_family      | VARCHAR |
+        | end_date         | DATE    |         +---------------------+---------+
+        +------------------+---------+
+ */
+ /*
+ PROMPT
+ -- The CMO is interested in understanding how the sales of different
+ -- product families are affected by promotional campaigns.
+ -- To do so, for each of the available product families,
+ -- show the total number of units sold,
+ -- as well as the ratio of units sold that had a valid promotion
+ -- to units sold without a promotion,
+ -- ordered by increasing order of total units sold.
+ 
+
+ EXPECTED OUTPUT
+ Note: Please use the column name(s) specified in the expected output in your solution.
+ +----------------+------------------+--------------------------------------------------+
+ | product_family | total_units_sold | ratio_units_sold_with_promo_to_sold_without_promo|
+ +----------------+------------------+--------------------------------------------------+
+ | Drink          |          43.0000 |                           0.79166666666666666667 |
+ | Non-Consumable |         176.0000 |                           0.76000000000000000000 |
+ | Food           |         564.0000 |                           0.75155279503105590062 |
+ +----------------+------------------+--------------------------------------------------+
+ 
+ -------------- PLEASE WRITE YOUR SQL SOLUTION BELOW THIS LINE ----------------
+ --qeustions to ask: how to tell if unit sold is with promo?
+ --answer: if promo_id <> 0
+ */
+ 
+
+ select product_family, 
+ sum(units_sold)*1.00 as total_units_sold, 
+ sum(case when promotion_id <> 0 then units_sold*1.0 else 0 end) / sum(case when promotion_id = 0 then units_sold*1.0 else 0 end)  as ratio_units_sold_with_promo_to_sold_without_prom
+ from 
+ (select s.promotion_id, s.units_sold, pc.product_family 
+   from sales as s
+   (left or)join products as p
+   on p.product_id  = s.product_id 
+   (left or )join product_classes as pc
+   on p.product_class_id = pc.product_class_id
+  )
+ group by 1 
+ order by total_units_sold
+
+
+
+  /*
+ BACKGROUND:
+ 
+ The following schema is a subset of a relational database of a grocery store
+ chain. This chain sells many products of different product classes to its
+ customers across its different stores. It also conducts many different
+ promotion campaigns.
+ 
+ The relationship between the four tables we want to analyze is depicted below:
+ 
+        # sales                                # products
+        +------------------+---------+         +---------------------+---------+
+        | product_id       | INTEGER |>--------| product_id          | INTEGER |
+        | store_id         | INTEGER |    +---<| product_class_id    | INTEGER |
+        | customer_id      | INTEGER |    |    | brand_name          | VARCHAR |
+   +---<| promotion_id     | INTEGER |    |    | product_name        | VARCHAR |
+   |    | store_sales      | DECIMAL |    |    | is_low_fat_flg      | TINYINT |
+   |    | store_cost       | DECIMAL |    |    | is_recyclable_flg   | TINYINT |
+   |    | units_sold       | DECIMAL |    |    | gross_weight        | DECIMAL |
+   |    | transaction_date | DATE    |    |    | net_weight          | DECIMAL |
+   |    +------------------+---------+    |    +---------------------+---------+
+   |                                      |
+   |    # promotions                      |    # product_classes
+   |    +------------------+---------+    |    +---------------------+---------+
+   +----| promotion_id     | INTEGER |    +----| product_class_id    | INTEGER |
+        | promotion_name   | VARCHAR |         | product_subcategory | VARCHAR |
+        | media_type       | VARCHAR |         | product_category    | VARCHAR |
+        | cost             | DECIMAL |         | product_department  | VARCHAR |
+        | start_date       | DATE    |         | product_family      | VARCHAR |
+        | end_date         | DATE    |         +---------------------+---------+
+        +------------------+---------+
+ */
+ /*
+ PROMPT:
+ -- The VP of Sales feels that some product categories don't sell
+ -- and can be completely removed from the inventory.
+ -- As a first pass analysis, they want you to find what percentage
+ -- of product categories have never been sold.
+ 
+ EXPECTED OUTPUT:
+ Note: Please use the column name(s) specified in the expected output in your solution.
+ +-----------------------------------+
+ | pct_product_categories_never_sold |
+ +-----------------------------------+
+ |               13.8888888888888889 |
+ +-----------------------------------+
+
+ -------------- PLEASE WRITE YOUR SQL SOLUTION BELOW THIS LINE ----------------
+
+ 
+ 
+ select * from sales*/
+ 
+--how to define percentage of product categories have never been sold?
+-- assume the persentage of distinct product count which can not found in sales table over total counts in products
+select (select count(*) from products where product_id not in (select product_id from sales)) * 1.0/(select count(distinct product_id) from products) as pct_product_categories_never_sold
+
+
+
+/*SELECT 100.0 * (1 - COUNT(DISTINCT product_category) / CAST((SELECT COUNT(DISTINCT product_category) FROM product_classes) AS FLOAT64)) AS pct_product_categories_never_sold
+FROM sales s JOIN products p ON (s.product_id=p.product_id) JOIN product_classes pc ON (p.product_class_id=pc.product_class_id)*/
+
+
+
+
+
+
+
+
