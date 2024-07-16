10b97470755c24f255f1a76644cdb440cef59320
Yonghui Li <liyonghui86@gmail.com>
2024-07-15 17:08:56 -0700

    add comments

--- binaryTree/leetcode543_DiameterofBinaryTree.py
+++ binaryTree/leetcode543_DiameterofBinaryTree.py
@@ -43,7 +43,9 @@ class Solution:
             left_depth = depth(node.left)
             right_depth = depth(node.right)
             curr_diameter = left_depth + right_depth
+            # Update the maximum diameter if the current diameter is larger
             self.max_diameter = max(self.max_diameter, curr_diameter)
+            # Return the height of the current node
             return 1+max(left_depth,right_depth)
         depth(root)
         return self.max_diameter
